716b0f01720ea4c5eeb5b92d3aa397d9a0440166
Your Name <you@example.com>
2022-03-24 16:34:15 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,34 @@
+function soma (numero1, numero2) {
+    let soma = numero1 + numero2;
+    console.log ('A soma dos dois números é: ' + soma + '')
+    return 'A soma dos dois números é: ' + soma;
+}
+
+soma (8, 4)
+
+
+function subtracao (numero1, numero2) {
+    let subtracao = numero1 - numero2;
+    console.log ('A subtração dos dois números é: ' + subtracao + '')
+    return 'A subtração dos dois números é: ' + subtracao;
+}
+
+subtracao (8, 4)
+
+
+function multiplicacao (numero1, numero2) {
+    let multiplicacao = numero1 * numero2;
+    console.log ('A multiplicação dos dois números é: ' + multiplicacao + '')
+    return 'A multiplicação dos dois números é: ' + multiplicacao;
+}
+
+multiplicacao (8, 4)
+
+
+function divisao (numero1, numero2) {
+    let divisao = numero1 / numero2;
+    console.log ('A divisao dos dois números é: ' + divisao + '')
+    return 'A divisão dos dois números é: ' + divisao;
+}
+
+divisao (8, 4)
